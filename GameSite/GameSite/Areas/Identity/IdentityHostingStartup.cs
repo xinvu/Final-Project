92b0497f5593e6684e498f15a919a4aa6cf8788c
Calvin <56831006+xinvu@users.noreply.github.com>
2020-04-30 01:36:11 -0500

    Added a login system

---
+++ GameSite/GameSite/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using GameSite.Areas.Identity.Data;
+using GameSite.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(GameSite.Areas.Identity.IdentityHostingStartup))]
+namespace GameSite.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AuthDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AuthDbContextConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>(options =>
+                {
+                    options.SignIn.RequireConfirmedAccount = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+
+                })
+                    .AddEntityFrameworkStores<AuthDbContext>();
+            });
+        }
+    }
+}
